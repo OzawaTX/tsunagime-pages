0b4f2bec5579fa3cd50ce486956448c00316d18a
OzawaTX <hidemasa.ozawa@gmail.com>
2025-10-09 21:59:31 +0900

    chore(middleware): add X-Functions-Rev=2025-10-09-v3

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,20 +1,18 @@
+// Cloudflare Pages Middleware (common headers for static & functions)
 export const onRequest: PagesFunction = async ({ request, next }) => {
-  // 下流の関数/静的を実行
   const res = await next();
 
-  // 共通ヘッダ（静的/動的すべてに付与）
-  res.headers.set("X-Tsunagime-Functions", "yes");
+  // 共通ヘッダ
+  res.headers.set("X-From-Middleware", "yes");
   res.headers.set("X-Robots-Tag", "noai, noimageai");
-  res.headers.set("X-Functions-Rev", "2025-10-09-v2");
-res.headers.set("tdm-reservation", "1");
+  res.headers.set("tdm-reservation", "1");
+  res.headers.set("X-Functions-Rev", "2025-10-09-v3");
 
   // /posts/ 以下には識別ヘッダ
   const { pathname } = new URL(request.url);
   if (pathname.startsWith("/posts/")) {
-    res.headers.set("X-From-Posts-Function", "yes");
+    res.headers.set("X-From-Posts-Function", res.headers.get("X-From-Posts-Function") ?? "yes");
   }
+
   return res;
 };
-
-
-
