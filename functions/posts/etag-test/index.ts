c6f3b9446981947cf9c246c1f7f8090a98ee96b8
OzawaTX <hidemasa.ozawa@gmail.com>
2025-10-13 00:34:11 +0900

    test(posts): add literal /posts/etag-test variants (200) to trigger middleware ETag/304

---
+++ functions/posts/etag-test/index.ts
@@ -0,0 +1,9 @@
+export const onRequest: PagesFunction = async () => {
+  const h = new Headers({
+    "X-From-Posts-Function": "yes",
+    "X-Test-Which": "posts/etag-test index",
+    "Content-Type": "text/html; charset=utf-8",
+    "Cache-Control": "public, max-age=60, must-revalidate"
+  });
+  return new Response("<h1>ETag test OK</h1>", { status: 200, headers: h });
+};
