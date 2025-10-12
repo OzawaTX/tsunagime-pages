19a56e96e9cfc49579ca6d6fa4c57308e064b738
OzawaTX <hidemasa.ozawa@gmail.com>
2025-10-12 22:49:04 +0900

    feat(debug): add /debug/[[path]].ts catch-all (404 + no-store) and route include

--- functions/debug/[[path]].ts
+++ functions/debug/[[path]].ts
@@ -1,9 +1,8 @@
 export const onRequest: PagesFunction = async ({ request }) => {
-  const { pathname, search } = new URL(request.url);
+  const u = new URL(request.url);
   const h = new Headers({
     "X-Debug-Route": "debug/*@function",
-    "X-Pathname": pathname + (search || ""),
-    "X-From-Posts-Function": "",
+    "X-Pathname": u.pathname + (u.search || ""),
     "X-From-Middleware": "yes",
     "X-Robots-Tag": "noai, noimageai",
     "tdm-reservation": "1",
@@ -12,5 +11,5 @@ export const onRequest: PagesFunction = async ({ request }) => {
     "Pragma": "no-cache",
     "Expires": "0"
   });
-  return new Response("debug no-store (catch-all under /debug)", { status: 404, headers: h });
+  return new Response("debug no-store (from /debug/[[path]].ts)", { status: 404, headers: h });
 };
