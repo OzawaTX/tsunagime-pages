ea812e8c2593862fb038cdde7f7006d1f122244d
OzawaTX <hidemasa.ozawa@gmail.com>
2025-10-13 00:50:10 +0900

    chore(test): remove ok200 & etag-test variants (cleanup)

--- functions/posts/etag-test.ts
+++
@@ -1,9 +0,0 @@
-export const onRequest: PagesFunction = async () => {
-  const h = new Headers({
-    "X-From-Posts-Function": "yes",
-    "X-Test-Which": "posts/etag-test literal",
-    "Content-Type": "text/html; charset=utf-8",
-    "Cache-Control": "public, max-age=60, must-revalidate"
-  });
-  return new Response("<h1>ETag test OK</h1>", { status: 200, headers: h });
-};
--- functions/posts/ok200.ts
+++
@@ -1,9 +0,0 @@
-export const onRequest: PagesFunction = async () => {
-  const h = new Headers({
-    "X-From-Posts-Function": "yes",
-    "X-Test-Which": "posts/ok200",
-    "Content-Type": "text/plain; charset=utf-8",
-    "Cache-Control": "no-store"
-  });
-  return new Response("posts ok", { status: 200, headers: h });
-};
