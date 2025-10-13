ce3586c8171472879bcd1b6240d4ee662f7406d0
OzawaTX <hidemasa.ozawa@gmail.com>
2025-10-13 22:55:20 +0900

    test(posts): prioritize ok200 by temporarily disabling posts/[id].ts

--- functions/posts/ok200/index.ts
+++ functions/posts/ok200/index.ts
@@ -1,9 +1,10 @@
 export const onRequest: PagesFunction = async () => {
   const h = new Headers({
     "X-From-Posts-Function": "yes",
-    "X-Test-Which": "posts/ok200",
+    "X-Test-Which": "posts/ok200 v2",
     "Content-Type": "text/plain; charset=utf-8",
     "Cache-Control": "public, max-age=60, must-revalidate"
   });
   return new Response("ok200", { status: 200, headers: h });
 };
+
