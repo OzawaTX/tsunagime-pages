6aac01725b5078439c2364cb452c9d09aba500ae
OzawaTX <hidemasa.ozawa@gmail.com>
2025-10-13 22:25:34 +0900

    chore(test): remove posts/ok200

--- functions/posts/ok200/index.ts
+++
@@ -1,9 +0,0 @@
-export const onRequest: PagesFunction = async () => {
-  const h = new Headers({
-    "X-From-Posts-Function": "yes",
-    "Content-Type": "text/plain; charset=utf-8",
-    // ミドルウェアのETag/LM付与と独立
-    "Cache-Control": "public, max-age=60, must-revalidate"
-  });
-  return new Response("ok200", { status: 200, headers: h });
-};
