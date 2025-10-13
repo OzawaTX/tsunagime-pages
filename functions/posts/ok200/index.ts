448d0295be09931e0bf4fb0f9ccbb22cd8e0ceb8
OzawaTX <hidemasa.ozawa@gmail.com>
2025-10-13 23:49:54 +0900

    chore: ensure /posts/* routed to Functions; add posts/ok200 literal+index; move public/posts aside

--- functions/posts/ok200/index.ts
+++ functions/posts/ok200/index.ts
@@ -1,12 +1,9 @@
 export const onRequest: PagesFunction = async () => {
   const h = new Headers({
     "X-From-Posts-Function": "yes",
-    "X-Test-Which": "posts/ok200 v2 bump v2",
+    "X-Test-Which": "posts/ok200",
     "Content-Type": "text/plain; charset=utf-8",
     "Cache-Control": "public, max-age=60, must-revalidate"
   });
   return new Response("ok200", { status: 200, headers: h });
 };
-
-
-
