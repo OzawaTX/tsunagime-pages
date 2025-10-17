24933de41893e4d16fe1b01fd022ad8a7ad657f0
OzawaTX <hidemasa.ozawa@gmail.com>
2025-10-18 01:21:33 +0900

    chore(posts): restore [id].ts and remove ok200 test handlers

--- functions/ok200.ts
+++
@@ -1,8 +0,0 @@
-export const onRequest: PagesFunction = async () => {
-  const h = new Headers({
-    "X-From-Function": "root-ok200",
-    "Content-Type": "text/plain; charset=utf-8",
-    "Cache-Control": "no-store"
-  });
-  return new Response("root ok", { status: 200, headers: h });
-};
